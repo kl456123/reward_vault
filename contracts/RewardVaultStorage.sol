87b43286c7a5ea97af6c4282db3ef4f413266412
break.x <break.x@binance.com>
2024-11-25 15:30:35 +0800

    fix storage layout

--- contracts/RewardVaultStorage.sol
+++ contracts/RewardVaultStorage.sol
@@ -2,6 +2,10 @@
 pragma solidity ^0.8.24;
 
 contract RewardVaultStorage {
+    // projectId => token balance
+    // Deprecated: dont need balanace sheet onchain
+    mapping(uint256 => mapping(address token => uint256))
+        internal _allProjectBalances;
     // hash => isRequestIdUsed
     mapping(uint256 => bool) internal isRequestIdUsed;
 }
